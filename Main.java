3badf0f4d2ee1d4c05622118e7c8e0572671c141
Chatrughan Prasad <chatrughan.prasad@outlook.com>
2023-04-23 11:32:41 +0530

    This the N queen problem - backtracking algorithm

--- Main.java
+++ Main.java
@@ -1,20 +1,61 @@
-package com.company;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        int[] queens = new int[n];
+        solveNQueensHelper(result, queens, 0, n);
+        return result;
+    }
+
+    private void solveNQueensHelper(List<List<String>> result, int[] queens, int row, int n) {
+        if (row == n) {
+            List<String> solution = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < n; j++) {
+                    if (queens[i] == j) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                solution.add(sb.toString());
+            }
+            result.add(solution);
+        } else {
+            for (int i = 0; i < n; i++) {
+                queens[row] = i;
+                if (isValid(queens, row)) {
+                    solveNQueensHelper(result, queens, row + 1, n);
+                }
+            }
+        }
+    }
+
+    private boolean isValid(int[] queens, int row) {
+        for (int i = 0; i < row; i++) {
+            if (queens[i] == queens[row]) {
+                return false;
+            }
+            if (Math.abs(queens[i] - queens[row]) == row - i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(12);
-        queue.offer(24);
-        queue.offer(89);
-        System.out.println(queue);
-        System.out.println(queue.poll());
-        System.out.println(queue);
-        System.out.println(queue.peek());
-
+        Main nQueens = new Main();
+        List<List<String>> solutions = nQueens.solveNQueens(4);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
     }
 
 }
